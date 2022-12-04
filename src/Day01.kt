fe965a913a4bccdbb7f770a6e32f1ecefd488854
Marcin Erbel <marcinerbel90@gmail.com>
2022-12-04 11:43:08 +0100

    Day1 - old solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,25 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int =
+            input.joinToString(",")
+                    .split(",,")
+                    .map { it.split(",").sumOf(String::toInt) }
+                    .maxOf { it }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>): Int =
+            input.joinToString(",")
+                    .split(",,")
+                    .map { it.split(",").sumOf(String::toInt) }
+                    .sortedDescending()
+                    .take(3)
+                    .sum()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val part1 = part1(testInput)
+    println(part1)
+    check(part1 == 67450)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val part2 = part2(testInput)
+    println(part2)
+    check(part2 == 199357)
 }
