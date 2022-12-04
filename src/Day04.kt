a8320504be93dfba1f634413a50e7240d16ba6d9
Marcin Erbel <marcinerbel90@gmail.com>
2022-12-04 13:40:43 +0100

    Day4 - Done

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,17 +1,33 @@
 fun main() {
 
-    fun part1(input: List<String>): Int =
-            input.size
+    fun part1(input: List<String>) = input
+            .map { it.split(",") }
+            .map {
+                val first = it[0].split("-")
+                val firstRange = IntRange(first[0].toInt(), first[1].toInt())
+                val second = it[1].split("-")
+                val secondRange = IntRange(second[0].toInt(), second[1].toInt())
+                (firstRange.contains(secondRange.first) && firstRange.contains(secondRange.last)) ||
+                        (secondRange.contains(firstRange.first) && secondRange.contains(firstRange.last))
+            }.count { it }
 
-    fun part2(input: List<String>): Int =
-            input.size
+    fun part2(input: List<String>): Int = input
+            .map { it.split(",") }
+            .map {
+                val first = it[0].split("-")
+                val firstRange = IntRange(first[0].toInt(), first[1].toInt())
+                val second = it[1].split("-")
+                val secondRange = IntRange(second[0].toInt(), second[1].toInt())
+                (firstRange.contains(secondRange.first) || firstRange.contains(secondRange.last)) ||
+                        (secondRange.contains(firstRange.first) || secondRange.contains(firstRange.last))
+            }.count { it }
 
     val testInput = readInput("Day04_test")
     val part1 = part1(testInput)
     println(part1)
-    check(part1 == 0)
+    check(part1 == 490)
 
     val part2 = part2(testInput)
     println(part2)
-    check(part2 == 0)
+    check(part2 == 921)
 }
